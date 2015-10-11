97b9246f304d481df048647cc6ce8437bf9c5d3b
productiveme <jaco@productive.me>
2015-10-11 08:17:54 +0200

    Recording votes

--- client/voting.js
+++ client/voting.js
@@ -1,13 +1,16 @@
-function likeCard($ctrl) {
+function likeCard($ctrl, vote) {
   $ctrl.addClass('rotate-left').delay(700).fadeOut(1);
   $('.post').find('.status').remove();
   $ctrl.append('<div class="status like">Like!</div>');
+
+  return Votes.insert(vote);
 }
 
-function unlikeCard($ctrl) {
+function unlikeCard($ctrl, vote) {
   $ctrl.addClass('rotate-right').delay(700).fadeOut(1);
   $('.post').find('.status').remove();
   $ctrl.append('<div class="status dislike">Dislike!</div>');
+  return Votes.insert(vote);
 }
 
 Template.voting.helpers({
@@ -18,9 +21,29 @@ Template.voting.helpers({
 
 Template.voting.events({
   "swiperight .post": function(ev, tmpl) {
-    likeCard($(ev.currentTarget));
+    var vote = {
+      user: {
+        _id: Meteor.userId()
+      },
+      post: {
+        _id: this._id
+      },
+      voted: 1,
+      timestamp: new Date()
+    };
+    likeCard($(ev.currentTarget), vote);
   },
   "swipeleft .post": function(ev, tmpl) {
-    unlikeCard($(ev.currentTarget));
+    var vote = {
+      user: {
+        _id: Meteor.userId()
+      },
+      post: {
+        _id: this._id
+      },
+      voted: -1,
+      timestamp: new Date()
+    };
+    unlikeCard($(ev.currentTarget), vote);
   }
 });
